f9e1d7c996b9c8e08c17afd91d40ea72f609de39
青木優音 <130417071+yutoaoki412@users.noreply.github.com>
2024-09-01 19:06:42 +0900

    utilsの関数を少しリファクタリング

--- utils.py
+++ utils.py
@@ -8,12 +8,11 @@ with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
 client = OpenAI(api_key=config['openai']['api_key'])
+credentials = Credentials.from_service_account_file(config['google']['service_account_file'])
+google_client = build('drive', 'v3', credentials=credentials)
 
 def get_docs_list(folder_id):
-    credentials = Credentials.from_service_account_file(config['google']['service_account_file'])
-    drive_service = build('drive', 'v3', credentials=credentials)
-
-    files = drive_service.files().list(
+    files = google_client.files().list(
         q=f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.document'",
         fields="files(id, name)"
     ).execute().get('files', [])
@@ -21,7 +20,7 @@ def get_docs_list(folder_id):
     docs_list = []
     for file in files:
         doc_id = file['id']
-        doc_content = drive_service.files().export(fileId=doc_id, mimeType='text/plain').execute()
+        doc_content = google_client.files().export(fileId=doc_id, mimeType='text/plain').execute()
         docs_list.append({
             'name': file['name'],
             'url': f"https://docs.google.com/document/d/{doc_id}/edit",
@@ -33,7 +32,7 @@ def get_docs_list(folder_id):
 def vectorize_text(text):
     response = client.embeddings.create(
         input=text,
-        model="text-embedding-3-large"
+        model=config['openai']['embedding_model']
     )
     return response.data[0].embedding
 
@@ -56,7 +55,7 @@ def ask_question(question, context):
     ]
     
     response = client.chat.completions.create(
-        model="gpt-4o-mini",
+        model=config['openai']['chat_model'],
         messages=messages
     )
 
